88f746a5bdf0335eecdb2d7ac18f42702d58a214
Adityayadav9 <146486403+Adityayadav9@users.noreply.github.com>
2025-06-11 12:34:43 +0000

    control

--- .devcontainer/control/one.js
+++ .devcontainer/control/one.js
@@ -1,9 +1,18 @@
-const IsUserLoggedIn = true
-const temperature = 50
-if(temperature>41){
-    console.log("temperature is greater tha 40");
+// const IsUserLoggedIn = true
+// const temperature = 50
+// if(temperature>41){
+//     console.log("temperature is greater tha 40");
 
-}
-else{
-    console.log("tempwrature is 50");
-}
+// }
+// else{
+//     console.log("tempwrature is 50");
+// }
+// console.log("Execute");
+// // <,>,<=,>=,==,===
+
+// const score = 200
+// if(score > 100){
+//   const power = "fly"
+//     console.log(`user power: ${power} `)
+// }
+// console.log(`user power: ${power}`) 
