e90bba9a53e201226d7f475e78bf08926fe1991d
Adityayadav9 <146486403+Adityayadav9@users.noreply.github.com>
2025-06-12 11:44:11 +0000

    switch

---
+++ .devcontainer/control/switch.js
@@ -0,0 +1,77 @@
+// switch(key){
+//     case Value:
+
+//     break;
+//     default:
+//         break;
+// }
+
+// const month = "march"
+// switch(month){
+//     case "jan":
+//         console.log("january");
+//         break;
+//         case "feb":
+//             console.log("feb");
+//             break;
+//             case "march":
+//                 console.log("march");
+//                 break;
+//                 case "april":
+//                     console.log("april");
+//                     break;
+//                     default :
+//                     console.log("default case match");
+//                     break;
+                
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const month = "march"
+// switch(month){
+//     case value:
+//         break;
+//         default:
+//             break;
+// }
+
+
+
+
+
+const month = "march"
+switch(month){
+    case "jan":
+        console.log("jan");
+        break;
+        case "feb":
+            console.log("fab");
+            break;
+            case "march":
+            console.log("march");
+            break;
+            case "april":
+                console.log("april");
+                break;
+                default:
+                    console.log("case is not matched");
+                    break;
+}
+
+
+
